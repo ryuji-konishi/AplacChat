4b89cff312f3878155441924c0c0000c7fd654a6
ryuji-konishi <ryuji.konishi@gmail.com>
2017-12-07 08:05:46 +1100

    Local directory samples added

--- chat/run_infer_web.py
+++ chat/run_infer_web.py
@@ -10,8 +10,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--out_dir", type=str, default=None, help="Store log/model files.")
 FLAGS, unparsed = parser.parse_known_args()
 if not FLAGS.out_dir:
-    FLAGS.out_dir = "/Users/ryuji/tmp/aplac/model"
-    FLAGS.out_dir = "/home/apps/prg/aplac/chat/generated/4_2316/model"
+    FLAGS.out_dir = "/Users/ryuji/prg/aplac/chat/generated/4_2316/model"	# MacBookAir13
+    FLAGS.out_dir = "/home/apps/prg/aplac/chat/generated/4_2316/model"	# AWS EC2
 controller.init(FLAGS.out_dir)
 
 #Do not add debug=True when VSCode is used. Otherwise breakpoint doesn't hit.
