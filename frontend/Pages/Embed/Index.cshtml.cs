5d3e4705b3fd7ee8d7daf73c8f9ab450ca56a796
ryuji-konishi <ryuji.konishi@gmail.com>
2018-02-16 16:44:46 +1100

    HTML framing is introduced. '/Embed/Index' returns iframe content and that is the main part of chatting. This is to be aimed at the future when this chat box is embedded in other web sites.

--- frontend/Pages/Embed/Index.cshtml.cs
+++ frontend/Pages/Embed/Index.cshtml.cs
@@ -4,15 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace frontend.Pages
+namespace frontend.Pages.Embed
 {
-    public class ContactModel : PageModel
+    public class IndexModel : PageModel
     {
-        public string Message { get; set; }
-
         public void OnGet()
         {
-            Message = "Your contact page.";
+
         }
     }
 }
