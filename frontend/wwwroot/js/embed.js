aef1ff1e54ae45483af22cc75c2b9bc56010aaab
ryuji-konishi <ryuji.konishi@gmail.com>
2018-02-21 18:41:46 +1100

    Refinement.

--- frontend/wwwroot/js/embed.js
+++ frontend/wwwroot/js/embed.js
@@ -1,5 +1,5 @@
-var init = function(form, scroll, records) {
-    var input = form.find('#chat-input')
+var init = function(divForm, divScroll, divRecords) {
+    var input = divForm.find('#chat-input')
 	input.keypress(function(e) {
 		var code = (e.keyCode ? e.keyCode : e.which);
 		if ((code == 8)	// BS or delete
@@ -11,39 +11,38 @@ var init = function(form, scroll, records) {
 		switch (code) {
 		case 13:	// enter
 			e.preventDefault();
-            submitChat(form, scroll, records);
+            submitChat(divForm, divScroll, divRecords);
 			break;
 		}
 	});
 	input.focus();
 }
 
-var initLayout = function(body, form, scroll) {
+var initLayout = function(body, divForm, divScroll) {
 	var totalHeight = body.height()
-	var formHeight = form.height()
+	var formHeight = divForm.height()
 	// Set the height of scroll div based on the total height (HTML body)
 	// and the form div where the form div height is constant.
 	// This work is required to make the scroll div scrollable. To make div
 	// scrollable its height needs to be explicitly set while 'overflow' is
 	// set to 'auto' in CSS.
 	var h = totalHeight - formHeight;
-	scroll.height(h)
+	divScroll.height(h)
 	// Setting 'max-height' is not for scroll div but its child div.
 	// The child div's max-height is set to 'inherit' so eventually
 	// the child div inherits the same value in max-height.
 	// By setting a distinct height, the child div can be aligned at bottom.
-	scroll.css('max-height', h)
+	divScroll.css('max-height', h)
 }
 
-var submitChat = function(form, scroll, records) {
-    var input = form.find('#chat-input')
+var submitChat = function(divForm, divScroll, divRecords) {
+    var input = divForm.find('#chat-input')
 	var text = input.val();
 	text = text.trim();
 	if (text == '')
 		return;
-	appendChatSend(scroll, records, text)
-	input.attr("placeholder", "");
-	showBusyImage(form);
+	appendChatSend(divScroll, divRecords, text)
+	showBusyImage(divForm);
 	$.ajax({
 		url : "http://localhost:5000/infer",
 		type : "POST",
@@ -52,33 +51,33 @@ var submitChat = function(form, scroll, records) {
 		data : text,
 		success : function(resp) {
 			if (resp != null) {
-                hideMessage(form);
+                hideMessage(divForm);
                 input.val('');
-				appendChatReceive(scroll, records, resp)
+				appendChatReceive(divScroll, divRecords, resp)
 			}
 			else {
-				showMessage(form, "<strong>Sorry!</strong> Internal error happened.");
+				showMessage(divForm, "<strong>Sorry!</strong> Internal error happened.");
 			}
-			hideBusyImage(form);
+			hideBusyImage(divForm);
 		},
 		error : function(xhr, status, error) {
-			showMessage(form, "<strong>Sorry!</strong> Internal error happened.");
-			hideBusyImage(form);
+			showMessage(divForm, "<strong>Sorry!</strong> Internal error happened.");
+			hideBusyImage(divForm);
 		}
 	});
 }
 
-var appendChatSend = function(scroll, records, text) {
-	appendChatText(scroll, records, text, 'chat-send-record', 'chat-send-decorate');
+var appendChatSend = function(divScroll, divRecords, text) {
+	appendChatText(divScroll, divRecords, text, 'chat-send-record', 'chat-send-decorate');
 }
 
-var appendChatReceive = function(scroll, records, text) {
-	appendChatText(scroll, records, text, 'chat-receive-record', 'chat-receive-decorate');
+var appendChatReceive = function(divScroll, divRecords, text) {
+	appendChatText(divScroll, divRecords, text, 'chat-receive-record', 'chat-receive-decorate');
 }
 
-var appendChatText = function(scroll, records, text, classNameRec, classNameDec) {
+var appendChatText = function(divScroll, divRecords, text, classNameRec, classNameDec) {
     // Append to the current HTML content so that the latest text comes at bottom.
-	var htm = records.html();	// Current HTML content.
+	var htm = divRecords.html();	// Current HTML content.
 	
 	// Decorate div. This div contains the text and style formatted.
     var decDiv = '<div class="' + classNameDec + '">';
@@ -91,27 +90,27 @@ var appendChatText = function(scroll, records, text, classNameRec, classNameDec)
 	outDiv += decDiv;
 	outDiv += '</div>';
 
-	records.html(htm + outDiv);
+	divRecords.html(htm + outDiv);
 	
 	// Scroll down the div section to the bottom so that the last record
 	// is shown at the bottom.
-	scroll.scrollTop(Number.MAX_SAFE_INTEGER)
+	divScroll.scrollTop(Number.MAX_SAFE_INTEGER)
 }
 
-var showMessage = function(form, message) {
-	form.find('#show-message').show().html(
+var showMessage = function(divForm, message) {
+	divForm.find('#show-message').show().html(
 			'<p><b>' + message + '</b></p>');
 }
 
-var hideMessage = function(form) {
-	form.find('#show-message').hide();
+var hideMessage = function(divForm) {
+	divForm.find('#show-message').hide();
 }
 
-var showBusyImage = function(form) {
-    form.find('#chat-input').addClass('chat-input-busy');
+var showBusyImage = function(divForm) {
+    divForm.find('#chat-input').addClass('chat-input-busy');
 }
 
-var hideBusyImage = function(form) {
-	form.find('#chat-input').removeClass('chat-input-busy');
+var hideBusyImage = function(divForm) {
+	divForm.find('#chat-input').removeClass('chat-input-busy');
 }
 
