225b3f9fe5389393abeb746bfc05d2650a5818c3
Ryuji Konishi <ryuji.konishi@gmail.com>
2018-03-07 05:53:53 +0000

    Chat generated file directory changed from Mac to AWS.

--- chat/run_infer_web.py
+++ chat/run_infer_web.py
@@ -16,8 +16,8 @@ nmt.add_arguments(parser)
 FLAGS, unparsed = parser.parse_known_args()
 
 if not FLAGS.out_dir:
-    FLAGS.out_dir = "/Users/ryuji/prg/aplac/chat/generated/4_2316/model"	# MacBookAir13
-#    FLAGS.out_dir = "/home/apps/prg/aplac/chat/generated/4_2316/model"	# AWS EC2
+#    FLAGS.out_dir = "/Users/ryuji/prg/aplac/chat/generated/4_2316/model"	# MacBookAir13
+    FLAGS.out_dir = "/home/apps/prg/aplac/chat/generated/4_2316/model"	# AWS EC2
 controller.init(FLAGS)
 
 #Do not add debug=True when VSCode is used. Otherwise breakpoint doesn't hit.
