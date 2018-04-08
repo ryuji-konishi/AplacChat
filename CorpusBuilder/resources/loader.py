c4bfe9dd70551e89618bd3cb38c2b1f2b6874b06
ryuji-konishi <ryuji_konishi@hotmail.com>
2018-04-08 16:16:48 +1000

    Joyo-kanji resource is added.

--- CorpusBuilder/resources/loader.py
+++ CorpusBuilder/resources/loader.py
@@ -1,24 +1,33 @@
 # -*- coding: utf-8 -*-
 import json
 import os, sys
-
+import csv
 
 class Loader(object):
-    def __init__(self, folder_name, file_name):
+    def __init__(self, folder_name):
         self.current_dir = os.path.dirname(os.path.realpath(__file__))
         self.folder = folder_name
-        self.file = file_name
 
-    def load_data(self):
-        filepath = os.path.join(self.current_dir, self.folder, self.file)
+    def _get_file_path(self, file_name):
+        return os.path.join(self.current_dir, self.folder, file_name)
+        
+    def load_json(self, file_name):
+        filepath = self._get_file_path(file_name)
         with open(filepath, "r", encoding='utf8') as fp:
             data = json.load(fp)
         return data
 
-    def save_data(self, data):
-        filepath = os.path.join(self.current_dir, self.folder, self.file)
+    def save_json(self, file_name, data):
+        filepath = self._get_file_path(file_name)
         with open(filepath, 'w', encoding='utf8') as fp:
             json.dump(data, fp, ensure_ascii=False)
+
+    def load_csv(self, file_name):
+        filepath = self._get_file_path(file_name)
+        with open(filepath, "r", encoding='utf8') as fp:
+            reader = csv.reader(fp)
+            data = list(reader)
+        return data
 
     def remove_duplicate(self, lst):
         """ Remove duplicates from the given list. """
@@ -28,19 +37,50 @@ class NameLoader(Loader):
     """ Name resource loader class.
     """
     def __init__(self):
-        Loader.__init__(self, "name", "names_jpn.json")
+        Loader.__init__(self, "name")
+        self.file_json = "names_jpn.json"
 
     def load_names(self):
         """ Load and return a table of lastname and firstname. """
-        data = self.load_data()
+        data = self.load_json(self.file_json)
         return data['lastname'], data['firstname']
 
     def refresh(self):
-        data = self.load_data()
+        data = self.load_json()
         lastnames = data['lastname']
         data['lastname'] = self.remove_duplicate(lastnames)
         firstnames = data['firstname']
         data['firstname'] = self.remove_duplicate(firstnames)
-        self.save_data(data)
+        self.save_json(self.file_json, data)
+
+class JoyoKanjiLoader(Loader):
+    """ Joyo-kanji resource loader class.
+    """
+    def __init__(self):
+        Loader.__init__(self, "joyo-kanji-code")
+        self.file_json = "joyo_kanji_code.json"
+        self.file_csv = "joyo-kanji-code-u.csv"
+        # Check if json file exists. If not, generate csv file from it.
+        # This is to run only once in the development phase.
+        json_path = self._get_file_path(self.file_json)
+        if not os.path.isfile(json_path):
+            buf = []
+            csv_data = self.load_csv(self.file_csv)
+            for row in csv_data:
+                c = row[0]
+                if c[0] == '#':     # if the line starts from '#', comment out.
+                    continue
+                buf.append(c)
+            self.save_json(self.file_json, buf)
+
+    def load_kanjis(self):
+        """ Load and return a list of Joyo-Kanji charactors. """
+        data = self.load_json(self.file_json)
+        return data
+
+    def refresh(self):
+        data = self.load_json()
+        data = self.remove_duplicate(data)
+        self.save_json(self.file_json, data)
 
 
