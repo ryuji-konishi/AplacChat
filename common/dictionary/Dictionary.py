2d358de8788d1214e4e8bca7ee88876090e612df
ryuji-konishi <ryuji_konishi@hotmail.com>
2018-04-08 16:07:20 +1000

    The container data type is changed from map to set for better performance.

--- common/dictionary/Dictionary.py
+++ common/dictionary/Dictionary.py
@@ -17,6 +17,9 @@ class Dictionary(object):
         else:
             return False
 
+    def get_data(self):
+        return self.delegate.get_data()
+
 class English(object):
     """ English dictionary class. The dictionary data is originated from https://github.com/dwyl/english-words.git
     """
@@ -28,25 +31,33 @@ class English(object):
         # Load dictionary file
         current_dir = os.path.dirname(os.path.realpath(__file__))
         self.filename = os.path.join(current_dir, "English", "words_dictionary.json")
-        self.words_map = None
+        self.words = None
 
     def _lazy_loading(self):
-        """ Initialize self.words_map value by reading file. This is time and memory consuming, so
-            do this only when self.words_map is required for the first time (lazy loading)
+        """ Initialize self.words value by reading file. This is time and memory consuming, so
+            do this only when self.words is required for the first time (lazy loading)
         """
-        if not self.words_map:
+        if not self.words:
+            self.words = set()
             with open(self.filename, "r") as english_dictionary:
-                self.words_map = json.load(english_dictionary)
+                words_map = json.load(english_dictionary)
+            # Load words from map
+            for key in words_map:
+                self.words.add(key)
             # Add some more
             for word in self.addons:
-                self.words_map[word] = 1
+                self.words.add(word)
+
+    def get_data(self):
+        self._lazy_loading()
+        return self.words
 
     def Check(self, word):
         self._lazy_loading()
         
         word = word.lower()
         # All the words are assigned with 1 in the dictionary.
-        if word in self.words_map:
-            return self.words_map[word] == 1
+        if word in self.words:
+            return True
         else:
             return False
