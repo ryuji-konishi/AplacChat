93b56b23e1d26682f273f02c141935d570b3ea54
ryuji-konishi <ryuji_konishi@hotmail.com>
2018-04-11 06:02:49 +1000

    city resource is added.

--- CorpusBuilder/resources/loader.py
+++ CorpusBuilder/resources/loader.py
@@ -3,7 +3,7 @@ import json
 import os, sys
 import csv
 
-class Loader(object):
+class _ResourceLoader(object):
     def __init__(self, folder_name):
         self.current_dir = os.path.dirname(os.path.realpath(__file__))
         self.folder = folder_name
@@ -33,62 +33,14 @@ class Loader(object):
         """ Remove duplicates from the given list. """
         return list(set(lst))
 
-class NameLoader(Loader):
-    """ Name resource loader class.
-    """
-    def __init__(self):
-        Loader.__init__(self, "name")
-        self.file_json = "names_jpn.json"
-
-    def load_names(self):
-        """ Load and return a table of lastname and firstname. """
-        data = self.load_json(self.file_json)
-        return data['lastname'], data['firstname']
-
-    def refresh(self):
-        data = self.load_json(self.file_json)
-        lastnames = data['lastname']
-        data['lastname'] = self.remove_duplicate(lastnames)
-        firstnames = data['firstname']
-        data['firstname'] = self.remove_duplicate(firstnames)
-        self.save_json(self.file_json, data)
-
-class SaluteLoader(Loader):
-    """ Salute resource loader class.
+class _SingleColInitLoader(_ResourceLoader):
+    """ Single column, initializable resource loader.
+        The table has one column. If the text starts from '#', the row is ignored.
         The initial data is loaded from CSV file during development.
-    """
-    def __init__(self):
-        Loader.__init__(self, 'salute')
-        self.file_json = 'salute.json'
-        self.file_csv = 'salute.csv'
-        # Check if json file exists. If not, generate csv file from it.
-        # This is to run only once in the development phase.
-        json_path = self._get_file_path(self.file_json)
-        if not os.path.isfile(json_path):
-            data = []
-            csv_data = self.load_csv(self.file_csv)
-            for row in csv_data:
-                src = row[0]
-                tgt = row[1]
-                data.append([src, tgt])
-            self.save_json(self.file_json, data)
-
-    def load_salutes(self):
-        """ Load and return a set of source/target list. """
-        data = self.load_json(self.file_json)
-        return data
-
-    def refresh(self):
-        data = self.load_json(self.file_json)
-        data = self.remove_duplicate(data)
-        self.save_json(self.file_json, data)
-
-class KanjiLoader(Loader):
-    """ Joyo-kanji resource loader class.
-        The initial data is loaded from CSV file during development.
+        The updated data is stored into JSON file.
     """
     def __init__(self, folder_name, file_json, file_csv):
-        Loader.__init__(self, folder_name)
+        _ResourceLoader.__init__(self, folder_name)
         self.file_json = file_json
         self.file_csv = file_csv
         # Check if json file exists. If not, generate csv file from it.
@@ -104,24 +56,157 @@ class KanjiLoader(Loader):
                 data.append(c)
             self.save_json(self.file_json, data)
 
-    def load_kanjis(self):
-        """ Load and return a list of Joyo-Kanji charactors. """
-        data = self.load_json(self.file_json)
-        return data
+    def load(self):
+        return self.load_json(self.file_json)
 
     def refresh(self):
         data = self.load_json(self.file_json)
         data = self.remove_duplicate(data)
         self.save_json(self.file_json, data)
 
-class JoyoKanjiLoader(KanjiLoader):
+class _TwoColInitLoader(_ResourceLoader):
+    """ Two columns, initializable resource loader.
+        The table has two columns.
+        The initial data is loaded from CSV file during development.
+        The updated data is stored into JSON file.
+    """
+    def __init__(self, folder_name, file_json, file_csv):
+        _ResourceLoader.__init__(self, folder_name)
+        self.file_json = file_json
+        self.file_csv = file_csv
+        # Check if json file exists. If not, generate csv file from it.
+        # This is to run only once in the development phase.
+        json_path = self._get_file_path(self.file_json)
+        if not os.path.isfile(json_path):
+            data = []
+            csv_data = self.load_csv(self.file_csv)
+            for row in csv_data:
+                data.append([row[0], row[1]])
+            self.save_json(self.file_json, data)
+
+    def load(self):
+        return self.load_json(self.file_json)
+
+    def refresh(self):
+        data = self.load_json(self.file_json)
+        data = self.remove_duplicate(data)
+        self.save_json(self.file_json, data)
+
+class _MultiColInitLoader(_ResourceLoader):
+    """ Multi columns, initializable resource loader.
+        The table has multiple columns. The first col is src, the following cols are tgts.
+        The initial data is loaded from CSV file during development.
+        The updated data is stored into JSON file.
+    """
+    def __init__(self, folder_name, file_json, file_csv):
+        _ResourceLoader.__init__(self, folder_name)
+        self.file_json = file_json
+        self.file_csv = file_csv
+        # Check if json file exists. If not, generate csv file from it.
+        # This is to run only once in the development phase.
+        json_path = self._get_file_path(self.file_json)
+        if not os.path.isfile(json_path):
+            data = []
+            csv_data = self.load_csv(self.file_csv)
+            for row in csv_data:
+                for i in range(1,len(row)):
+                    data.append([row[0], row[i]])
+            self.save_json(self.file_json, data)
+
+    def load(self):
+        return self.load_json(self.file_json)
+
+    def refresh(self):
+        data = self.load_json(self.file_json)
+        data = self.remove_duplicate(data)
+        self.save_json(self.file_json, data)
+
+class NameLoader(_ResourceLoader):
+    """ Name resource loader class.
+    """
+    def __init__(self):
+        _ResourceLoader.__init__(self, "name")
+        self.file_json = "names_jpn.json"
+
+    def load_names(self):
+        """ Load and return a table sum of lastname and firstname. """
+        data = self.load_json(self.file_json)
+        lastnames = data['lastname']
+        firstnames = data['firstname']
+        lastnames.extend(firstnames)
+        return lastnames
+
+    def refresh(self):
+        data = self.load_json(self.file_json)
+        lastnames = data['lastname']
+        data['lastname'] = self.remove_duplicate(lastnames)
+        firstnames = data['firstname']
+        data['firstname'] = self.remove_duplicate(firstnames)
+        self.save_json(self.file_json, data)
+
+class SaluteLoader(object):
+    """ Salute resource loader class.
+        The initial data is loaded from CSV file during development.
+    """
+    def __init__(self):
+        self.delegate = _MultiColInitLoader('salute', 'salute.json', 'salute.csv')
+
+    def load_salutes(self):
+        """ Load and return a set of source/target list. """
+        return self.delegate.load()
+
+    def refresh(self):
+        self.delegate.refresh()
+
+class ConversationLoader(object):
+    """ Conversation resource loader class.
+        The initial data is loaded from CSV file during development.
+    """
+    def __init__(self):
+        self.delegate = _MultiColInitLoader('conversation', 'conversation.json', 'conversation.csv')
+
+    def load_conversations(self):
+        """ Load and return a set of source/target list. """
+        return self.delegate.load()
+
+    def refresh(self):
+        self.delegate.refresh()
+
+class JoyoKanjiLoader(object):
     """ Joyo-kanji resource loader class.
     """
     def __init__(self):
-        KanjiLoader.__init__(self, "kanji_joyo", "joyo_kanji_code.json", "joyo-kanji-code-u.csv")
+        self.delegate = _SingleColInitLoader('kanji_joyo', 'joyo_kanji_code.json', 'joyo-kanji-code-u.csv')
 
-class JinmeiKanjiLoader(KanjiLoader):
+    def load_kanjis(self):
+        """ Load and return a list of Joyo-Kanji charactors. """
+        return self.delegate.load()
+
+    def refresh(self):
+        self.delegate.refresh()
+
+class JinmeiKanjiLoader(object):
     """ Jinmeiyou-kanji resource loader class.
     """
     def __init__(self):
-        KanjiLoader.__init__(self, "kanji_jinmeiyo", "jinmei_kanji_code.json", "jinmeiyou-kanji-code-u.csv")
+        self.delegate = _SingleColInitLoader('kanji_jinmeiyo', 'jinmei_kanji_code.json', 'jinmeiyou-kanji-code-u.csv')
+
+    def load_kanjis(self):
+        """ Load and return a list of Joyo-Kanji charactors. """
+        return self.delegate.load()
+
+    def refresh(self):
+        self.delegate.refresh()
+
+class CityLoader(object):
+    """ City name resource loader class.
+    """
+    def __init__(self):
+        self.delegate = _SingleColInitLoader('city', 'city.json', None)
+
+    def load_cities(self):
+        """ Load and return a list of city names """
+        return self.delegate.load()
+
+    def refresh(self):
+        self.delegate.refresh()
